c41f63659ac3edfba5198d51f5396f4ba9d5c89c
UkanaBastreet <s.ppp@bk.ru>
2025-08-04 00:23:14 +0300

    refactor GameField and TextField components for improved structure and event handling

--- src/components/GameField/GameField.tsx
+++ src/components/GameField/GameField.tsx
@@ -1,29 +1,12 @@
-import { FC, useCallback, useEffect } from "react";
+import { FC } from "react";
 import { TextField } from "./TextField/TextField";
 import "./GameField.scss";
-import { Timer } from "./Timer/Timer";
-import { useDispatch } from "react-redux";
-import { keyPressed } from "src/store/slices/GameSlice";
 
 interface GameFieldProps {}
 
 export const GameField: FC<GameFieldProps> = () => {
-  const dispatch = useDispatch();
-  const keydownHandler = useCallback(
-    (e: KeyboardEvent) => {
-      dispatch(keyPressed(e.key));
-    },
-    [dispatch]
-  );
-  useEffect(() => {
-    window.addEventListener("keypress", keydownHandler);
-    return () => {
-      window.removeEventListener("keypress", keydownHandler);
-    };
-  }, [keydownHandler]);
   return (
     <div className="game-field">
-      <Timer />
       <TextField />
     </div>
   );
--- src/components/GameField/TextField/TextField.tsx
+++ src/components/GameField/TextField/TextField.tsx
@@ -1,22 +1,32 @@
-import { CSSProperties, FC, useEffect, useRef, useState } from "react";
+import {
+  CSSProperties,
+  FC,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import "./TextField.scss";
 import { RootState } from "src/store/store";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { keyPressed } from "src/store/slices/GameSlice";
 
 interface TextFieldProps {}
 export const TextField: FC<TextFieldProps> = () => {
-  const text = useSelector((state: RootState) => state.game.text);
-  const index = useSelector((state: RootState) => state.game.index);
-
-  const completedText = text.slice(0, index);
-  const currentChar = text[index];
-  const pendingText = text.slice(index + 1, text.length);
+  const { pendingText, currentChar, completedText } = useText();
   const [shift, setShift] = useState(0);
-
   const textStyles = { "--shift": shift } as CSSProperties;
+
+  const dispatch = useDispatch();
 
   const curRef = useRef<null | HTMLSpanElement>(null);
   const textRef = useRef<null | HTMLDivElement>(null);
+  const keydownHandler = useCallback(
+    (e: KeyboardEvent) => {
+      dispatch(keyPressed(e.key));
+    },
+    [dispatch]
+  );
   useEffect(() => {
     if (curRef.current && textRef.current) {
       const offsetTop = curRef.current.offsetTop;
@@ -25,7 +35,15 @@ export const TextField: FC<TextFieldProps> = () => {
         setShift((n) => n + 1);
       }
     }
-  }, [index, shift]);
+  }, [shift]);
+
+  useEffect(() => {
+    window.addEventListener("keypress", keydownHandler);
+    return () => {
+      window.removeEventListener("keypress", keydownHandler);
+    };
+  }, [keydownHandler]);
+
   return (
     <div className="text-field">
       <div className="text" ref={textRef} style={textStyles}>
@@ -37,4 +55,13 @@ export const TextField: FC<TextFieldProps> = () => {
       </div>
     </div>
   );
+};
+
+const useText = () => {
+  const text = useSelector((state: RootState) => state.game.text);
+  const index = useSelector((state: RootState) => state.game.index);
+  const completedText = text.slice(0, index);
+  const currentChar = text[index];
+  const pendingText = text.slice(index + 1, text.length);
+  return { completedText, currentChar, pendingText };
 };
