1fc0cf744850c22a5818dbd7ce9c2e26508f18b9
UkanaBastreet <s.ppp@bk.ru>
2025-08-03 21:45:29 +0300

    textField three lines text only + autoscroll

--- src/components/GameField/TextField/TextField.tsx
+++ src/components/GameField/TextField/TextField.tsx
@@ -1,6 +1,6 @@
-import { FC } from "react";
+import { CSSProperties, FC, useEffect, useRef, useState } from "react";
 import { inputError } from "src/types/game.type";
-import './TextField.scss'
+import "./TextField.scss";
 
 interface TextFieldProps {
   text: string;
@@ -11,11 +11,30 @@ export const TextField: FC<TextFieldProps> = ({ text, index, errors }) => {
   const completedText = text.slice(0, index);
   const currentChar = text[index];
   const pendingText = text.slice(index + 1, text.length);
+  const [shift, setShift] = useState(0);
+
+  const textStyles = { "--shift": shift } as CSSProperties;
+
+  const curRef = useRef<null | HTMLSpanElement>(null);
+  const textRef = useRef<null | HTMLDivElement>(null);
+  useEffect(() => {
+    if (curRef.current && textRef.current) {
+      const offsetTop = curRef.current.offsetTop;
+      console.log("offsetTop : ", offsetTop);
+      if (offsetTop > 43 + shift * 39) {
+        setShift((n) => n + 1);
+      }
+    }
+  }, [index, shift]);
   return (
     <div className="text-field">
-      <span className="completed-text">{completedText}</span>
-      <span className="current-char">{currentChar}</span>
-      <span className="pending-text">{pendingText}</span>
+      <div className="text" ref={textRef} style={textStyles}>
+        <span className="completed-text">{completedText}</span>
+        <span className="current-char" ref={curRef}>
+          {currentChar}
+        </span>
+        <span className="pending-text">{pendingText}</span>
+      </div>
     </div>
   );
 };
