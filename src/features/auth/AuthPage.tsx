cff71b6882cf050968a6e70710373bb3a5838759
UkanaBastreet <s.ppp@bk.ru>
2025-08-26 23:47:24 +0300

    animate mount auth page comp

--- src/features/auth/AuthPage.tsx
+++ src/features/auth/AuthPage.tsx
@@ -1,12 +1,17 @@
 import { LoginForm } from "./components/LoginForm/LoginForm";
 import { RegisterForm } from "./components/RegisterForm/RegisterForm";
 import styles from "./AuthPage.module.css";
+import { motion } from "framer-motion";
 
 export const AuthPage = () => {
   return (
-    <div className={styles.AuthPage}>
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      className={styles.AuthPage}
+    >
       <RegisterForm />
       <LoginForm />
-    </div>
+    </motion.div>
   );
 };
